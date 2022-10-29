dd3e6db55aac029a5b2ac569efbb72a8d3f37c64
shelepuginivan <shelepuginivanm@gmail.com>
2022-10-29 15:56:13 +0300

    bug fix

--- src/utils/config.js
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAnCBCq1nTHrSI3P0uozevbJ959ybXp2do",
-  authDomain: "calendar369-3e64a.firebaseapp.com",
-  projectId: "calendar369-3e64a",
-  storageBucket: "calendar369-3e64a.appspot.com",
-  messagingSenderId: "472082739163",
-  appId: "1:472082739163:web:26b88c00eab74f678e81fe",
-  measurementId: "G-TXLKGERGJD"
-};
