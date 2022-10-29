6298c7421a3efbd52b364312908c702fb3bc6a78
shelepuginivan <shelepuginivanm@gmail.com>
2022-10-29 19:22:48 +0300

    improved ui, added active page highlight

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,15 +9,24 @@ import {
 import {Link} from "react-router-dom";
 import '../css/header.css'
 
+
 const Header = () => {
     return localStorage.getItem('status') === 'student' ?
         (
           <header className="header">
+              <div className="options">
+                  <span className="icon-options"></span>
+                  <div className="options-links">
+                      <Link className="options-link" data-is-active={(document.location.pathname === INDEX_ROUTE).toString()} to={INDEX_ROUTE}>Главная</Link>
+                      <Link className="options-link" data-is-active={(document.location.pathname === ALL_EVENTS_ROUTE).toString()} to={ALL_EVENTS_ROUTE}>Все события</Link>
+                      <Link className="options-link" data-is-active={(document.location.pathname === USER_REGISTERED_EVENTS_ROUTE).toString()} to={USER_REGISTERED_EVENTS_ROUTE}>Я участвую</Link>
+                  </div>
+              </div>
               <div className="options"><span className="icon-options"></span></div>
-              <Link className="header-link" to={INDEX_ROUTE}>Главная</Link>
-              <Link className="header-link" to={ALL_EVENTS_ROUTE}>Все события</Link>
-              <Link className="header-link" to={USER_REGISTERED_EVENTS_ROUTE}>Я участвую</Link>
-              <Link className="header-link" to={PROFILE_ROUTE}>{localStorage.getItem('username')}</Link>
+              <Link className="header-link" data-is-active={(document.location.pathname === INDEX_ROUTE).toString()} to={INDEX_ROUTE}>Главная</Link>
+              <Link className="header-link" data-is-active={(document.location.pathname === ALL_EVENTS_ROUTE).toString()} to={ALL_EVENTS_ROUTE}>Все события</Link>
+              <Link className="header-link" data-is-active={(document.location.pathname === USER_REGISTERED_EVENTS_ROUTE).toString()} to={USER_REGISTERED_EVENTS_ROUTE}>Я участвую</Link>
+              <Link className="header-link" data-is-active={(document.location.pathname === PROFILE_ROUTE).toString()} to={PROFILE_ROUTE}>{localStorage.getItem('username')}</Link>
 
           </header>
         )
@@ -27,17 +36,17 @@ const Header = () => {
               <div className="options">
                   <span className="icon-options"></span>
                   <div className="options-links">
-                      <Link className="options-link" to={INDEX_ROUTE}>Главная</Link>
-                      <Link className="options-link" to={ALL_EVENTS_ROUTE}>Все события</Link>
-                      <Link className="options-link" to={MY_EVENTS_ROUTE}>Мои события</Link>
-                      <Link className="options-link" to={EVENT_EDITOR_ROUTE}>Создать событие</Link>
+                      <Link className="options-link" data-is-active={(document.location.pathname === INDEX_ROUTE).toString()} to={INDEX_ROUTE}>Главная</Link>
+                      <Link className="options-link" data-is-active={(document.location.pathname === ALL_EVENTS_ROUTE).toString()} to={ALL_EVENTS_ROUTE}>Все события</Link>
+                      <Link className="options-link" data-is-active={(document.location.pathname === MY_EVENTS_ROUTE).toString()} to={MY_EVENTS_ROUTE}>Мои события</Link>
+                      <Link className="options-link" data-is-active={(document.location.pathname === EVENT_EDITOR_ROUTE).toString()} to={EVENT_EDITOR_ROUTE}>Создать событие</Link>
                   </div>
               </div>
-              <Link className="header-link" to={INDEX_ROUTE}>Главная</Link>
-              <Link className="header-link" to={ALL_EVENTS_ROUTE}>Все события</Link>
-              <Link className="header-link" to={MY_EVENTS_ROUTE}>Мои события</Link>
-              <Link className="header-link" to={EVENT_EDITOR_ROUTE}>Создать событие</Link>
-              <Link className="header-link profile-link" to={PROFILE_ROUTE}>{localStorage.getItem('username')}</Link>
+              <Link className="header-link" data-is-active={(document.location.pathname === INDEX_ROUTE).toString()} to={INDEX_ROUTE}>Главная</Link>
+              <Link className="header-link" data-is-active={(document.location.pathname === ALL_EVENTS_ROUTE).toString()} to={ALL_EVENTS_ROUTE}>Все события</Link>
+              <Link className="header-link" data-is-active={(document.location.pathname === MY_EVENTS_ROUTE).toString()} to={MY_EVENTS_ROUTE}>Мои события</Link>
+              <Link className="header-link" data-is-active={(document.location.pathname === EVENT_EDITOR_ROUTE).toString()} to={EVENT_EDITOR_ROUTE}>Создать событие</Link>
+              <Link className="header-link profile-link" data-is-active={(document.location.pathname === PROFILE_ROUTE).toString()} to={PROFILE_ROUTE}>{localStorage.getItem('username')}</Link>
           </header>
         );
 
