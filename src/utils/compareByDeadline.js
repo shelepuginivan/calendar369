22e2280a9371f192e824edfac54bd58cfee6821d
shelepuginivan <shelepuginivanm@gmail.com>
2022-10-31 22:20:07 +0300

    events with the closest deadline are shown at the top now

--- src/utils/compareByDeadline.js
+++ src/utils/compareByDeadline.js
@@ -1,8 +1,8 @@
 export const compareByDeadline = (event1, event2) => {
         if (event1.deadline < event2.deadline) {
-            return 1
-        } else if (event1.deadline > event2.deadline) {
             return -1
+        } else if (event1.deadline > event2.deadline) {
+            return 1
         } else {
             return 0
         }
